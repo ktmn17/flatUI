3419f942b88e9258e10bbbd01cb3e2e95ed514f8
ktmn17 <hadilen@mail.ru>
2016-10-30 02:53:01 +0600

    did the main structure

--- style/block/info-circle/info-circle_percentage.js
+++ style/block/info-circle/info-circle_percentage.js
@@ -1,33 +1,35 @@
-var input_percentage_class = $('.info-circle__input-percentage');
+$(function() {
+    var input_percentage_class = $('.info-circle__input-percentage');
 
-function create_length_of_ring(input_class) {
+    function create_length_of_ring(input_class) {
 
-    var input_perc = +($(input_class).val());
-    if (input_perc == 0) $(input_class).val(0);
-    if (input_perc >= 100) {
-        input_perc = 100;
-        $(input_class).val(100);
-        $(input_class).css('font-size', '28px').css('top', '30%');
-    } else $(input_class).css('font-size', '40px').css('top', '25%');
+        var input_perc = +($(input_class).val());
+        if (input_perc == 0) $(input_class).val(0);
+        if (input_perc >= 100) {
+            input_perc = 100;
+            $(input_class).val(100);
+            $(input_class).css('font-size', '28px').css('top', '30%');
+        } else $(input_class).css('font-size', '40px').css('top', '25%');
 
-    var i = $(input_class).index('.info-circle__input-percentage');
-    var circle_radius = parseInt($('.info-circle__svg-ring_percentage').eq(i).css('r'));
-    var length_ring = circle_radius * 2 * 3.14;
+        var i = $(input_class).index('.info-circle__input-percentage');
+        var circle_radius = parseInt($('.info-circle__svg-ring_percentage').eq(i).css('r'));
+        var length_ring = circle_radius * 2 * 3.14;
 
-    $('.info-circle__svg-ring_percentage').eq(i)
-    .css('stroke-dasharray', input_perc * 0.01 * length_ring + ' ' + (length_ring - input_perc));
-}
-
-
-input_percentage_class.focusout(function() {
-    create_length_of_ring(this);
-});
-
-input_percentage_class.each(function(i) {
-    create_length_of_ring(input_percentage_class.eq(i));
-});
+        $('.info-circle__svg-ring_percentage').eq(i)
+            .css('stroke-dasharray', input_perc * 0.01 * length_ring + ' ' + (length_ring - input_perc));
+    }
 
 
-input_percentage_class.inputmask('999', {
-    placeholder: ''
+    input_percentage_class.focusout(function() {
+        create_length_of_ring(this);
+    });
+
+    input_percentage_class.each(function(i) {
+        create_length_of_ring(input_percentage_class.eq(i));
+    });
+
+
+    input_percentage_class.inputmask('999', {
+        placeholder: ''
+    });
 });
--- style/block/info-circle/info-circle_pie-chart.js
+++ style/block/info-circle/info-circle_pie-chart.js
@@ -1,32 +1,34 @@
-var input_percentage_class = $('.info-circle__input-pie-chart');
+$(function() {
+    var input_percentage_class = $('.info-circle__input-pie-chart');
 
-function create_length_of_pie_chart(input_class) {
+    function create_length_of_pie_chart(input_class) {
 
 
-    var input_perc = +($(input_class).val());
-    if (input_perc == 0) $(input_class).val(0);
-    if (input_perc >= 100) {
-        input_perc = 100;
-        $(input_class).val(100);
+        var input_perc = +($(input_class).val());
+        if (input_perc == 0) $(input_class).val(0);
+        if (input_perc >= 100) {
+            input_perc = 100;
+            $(input_class).val(100);
+        }
+
+        var i = $(input_class).index('.info-circle__input-pie-chart');
+        var circle_radius = parseInt($('.info-circle__svg-ring_pie-chart').eq(i).css('r'));
+        var length_ring = circle_radius * 2 * 3.14;
+
+        $('.info-circle__svg-ring_pie-chart').eq(i)
+            .css('stroke-dasharray', input_perc * 0.01 * length_ring + ' ' + (length_ring - input_perc));
     }
 
-    var i = $(input_class).index('.info-circle__input-pie-chart');    
-    var circle_radius = parseInt($('.info-circle__svg-ring_pie-chart').eq(i).css('r'));
-    var length_ring = circle_radius * 2 * 3.14;
 
-    $('.info-circle__svg-ring_pie-chart').eq(i)
-        .css('stroke-dasharray', input_perc * 0.01 * length_ring + ' ' + (length_ring - input_perc));
-}
+    input_percentage_class.each(function(i) {
+        create_length_of_pie_chart(input_percentage_class.eq(i));
+    });
 
+    input_percentage_class.focusout(function() {
+        create_length_of_pie_chart(this);
+    });
 
-input_percentage_class.each(function(i) {
-    create_length_of_pie_chart(input_percentage_class.eq(i));
-});
-
-input_percentage_class.focusout(function() {
-    create_length_of_pie_chart(this);
-});
-
-input_percentage_class.inputmask('999', {
-    placeholder: ''
+    input_percentage_class.inputmask('999', {
+        placeholder: ''
+    });
 });
